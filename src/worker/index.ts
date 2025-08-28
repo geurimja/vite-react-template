daccb0b3c1da22f49654b0435583287707d4d1ea
subekuy <kurobakaitou150@gmail.com>
2025-08-28 23:07:23 +0700

    Menambahkan CORS header endpoint /api/

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,9 +1,9 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/api/", (c) => c.json({ name: "Heya ni Youkoso" },
-{
-	headers: "Acces-Control-Allow-Origin": "*"
-}));
+app.get("/api/", (c) => {
+	c.header("Access-Control-Allow-Origin", "*")
+	return c.json({ name: "Heya ni Youkoso" })
+});
 
 export default app;
